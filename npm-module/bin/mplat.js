aa8da88c1a3359f3e49f5562f29390a96d67f0b5
Patrick Hallisey <hallipr@gmail.com>
2025-03-29 11:06:01 -0700

    Use prettier

--- npm-module/bin/mplat.js
+++ npm-module/bin/mplat.js
@@ -10,22 +10,22 @@ let platform = os.platform();
 const packageName = `@hallipr/mplat-${platform}-${os.arch()}`;
 
 function getPackagePath() {
-    const basePaths = require.resolve.paths(packageName) || [];
-    for (const basePath of basePaths) {
-        const packagePath = path.join(basePath, packageName);
-        if (fs.existsSync(packagePath)) {
-            return packagePath;
-        }
+  const basePaths = require.resolve.paths(packageName) || [];
+  for (const basePath of basePaths) {
+    const packagePath = path.join(basePath, packageName);
+    if (fs.existsSync(packagePath)) {
+      return packagePath;
     }
+  }
 
-    return null;
+  return null;
 }
 
 let packagePath = getPackagePath();
 
 if (!packagePath) {
-    console.error(`Platform package "${packageName}" is not installed.`);
-    process.exit(1); // Exit with an error code
+  console.error(`Platform package "${packageName}" is not installed.`);
+  process.exit(1); // Exit with an error code
 }
 
 console.log(`Using package: ${packagePath}`);
@@ -36,15 +36,15 @@ const childProcess = require('child_process');
 const execPath = path.join(packagePath, 'bin', 'mplat-cli');
 
 const child = childProcess.spawn(execPath, args, {
-    stdio: 'inherit',
-    shell: true,
+  stdio: 'inherit',
+  shell: true,
 });
 
 child.on('error', (err) => {
-    console.error(`Error executing package: ${err.message}`);
-    process.exit(1); // Exit with an error code
+  console.error(`Error executing package: ${err.message}`);
+  process.exit(1); // Exit with an error code
 });
 
 child.on('exit', (code) => {
-    process.exit(code); // Exit with the same code as the child process
+  process.exit(code); // Exit with the same code as the child process
 });
